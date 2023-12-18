800aac48d46776ab90199f851c0b2543df2bb339
XiChan <380639414@qq.com>
2023-12-19 01:49:53 +0800

    Vanilla

---
+++ src/components/OpenInNewTabSection.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input id="openInNewTabCheckbox" type="checkbox" v-model="settings.openInNewTab">
+        <label for="openInNewTabCheckbox">{{$t("openInNewTab")}}</label>
+    </div>
+</template>
+
+<script>
+import {settings} from "@/GlobalSetting.vue";
+
+export default {
+    name: "OpenInNewTabSection",
+    data() {
+        return {
+            settings,
+        };
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
